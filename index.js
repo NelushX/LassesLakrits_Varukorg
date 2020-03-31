2c33179ecb114146a248cd4af3d40c07cc30e31c
Hanna <[email address]>
2020-03-31 15:18:27 +0200

    stuff

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const app = express();
 
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }))
 
 app.use(sassMiddleware({
