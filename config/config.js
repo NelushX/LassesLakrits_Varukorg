c40ce321844479a9a63dffd4f44c64fe3ca885e3
Hanna <[email address]>
2020-03-31 11:34:13 +0200

    sass ändrinn

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV !== 'production') require("dotenv").config();
 
 const config = {
-    databaseURL: process.env.DATABASE,
+    databaseUrl: process.env.DATABASE,
     mail: process.env.MAIL
 }
 
